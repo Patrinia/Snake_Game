7607fd6f73cba02659643826e3ff5d252b0efbe5
Patrinia <Patriniafolia@naver.com>
2025-12-06 19:56:52 +0900

    개발 완료(HP바, 상태창, 보상시스템 완성)

--- app/src/main/java/com/example/myapplication/BattleActivity.kt
+++ app/src/main/java/com/example/myapplication/BattleActivity.kt
@@ -9,6 +9,7 @@ import android.view.animation.LinearInterpolator
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -25,14 +26,39 @@ class BattleActivity : AppCompatActivity() {
 
     private lateinit var btnRSPZone: LinearLayout
     private lateinit var diceZone: LinearLayout
-    private lateinit var diceImage: ImageView
     private lateinit var btnRollDice: ImageButton
 
+    private lateinit var statusWindow: LinearLayout
+
+    private lateinit var txtStatusHP: TextView
+    private lateinit var txtStatusAtk: TextView
+    private lateinit var txtStatusDef: TextView
+
+    private lateinit var txtStatusDiceCnt: TextView
+
+    private lateinit var btnStatus: ImageButton
+
+    private lateinit var rewardZone: LinearLayout
+    private lateinit var reward1: ImageButton
+    private lateinit var reward2: ImageButton
+    private lateinit var reward3: ImageButton
+
+    //HP 바 & 텍스트
+    private lateinit var playerHpBar: ProgressBar
+    private lateinit var enemyHpBar: ProgressBar
+    private lateinit var playerHpText: TextView
+    private lateinit var enemyHpText: TextView
+
+    private var isStatusOpen = false
     private var readyToBattle = false
 
     // ★ 전투 변수
-    private var playerHP = 20
-    private var enemyHP = 20
+    private val playerMaxHP = 20
+    private var playerHP = playerMaxHP
+
+
+    private val enemyMaxHP = 6
+    private var enemyHP = enemyMaxHP
 
     private var playerAtk = 0
     private var enemyAtk = 0
@@ -46,9 +72,19 @@ class BattleActivity : AppCompatActivity() {
     // ★ 턴 정보
     private var isPlayerTurn = true
 
-    // ★ 스팸 방지 Lock
-//    private var isDiceRolling = false
+    private val rewardList = listOf(
+        Reward("체력 +2", R.drawable.reward_hp, type = "hp", value = 2),
+        Reward("공격력 +1", R.drawable.reward_atk, type = "atk", value = 1),
+        Reward("방어력 +1", R.drawable.reward_def, type = "def", value = 1),
+        Reward("추가 주사위 +1", R.drawable.reward_dice, type = "dice", value = 1)
+    )
 
+    data class Reward(
+        val name: String,
+        val img: Int,
+        val type: String,
+        val value: Int
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,7 +100,7 @@ class BattleActivity : AppCompatActivity() {
 
         // 주사위 영역
         diceZone = findViewById(R.id.dicezone)
-        diceImage = findViewById(R.id.diceImage)
+//        diceImage = findViewById(R.id.diceImage)
         btnRollDice = findViewById(R.id.btnRollDice)
 
         // 선택 이미지
@@ -74,6 +110,15 @@ class BattleActivity : AppCompatActivity() {
         // 결과 텍스트 ("선공" / "후공" 및 전투 로그)
         whoFirst = findViewById(R.id.whofirst)
 
+        // 스테이터스창
+        statusWindow = findViewById(R.id.statusWindow)
+        txtStatusHP = findViewById(R.id.txtStatusPlayerHP)
+        txtStatusAtk = findViewById(R.id.txtStatusPlayerAtk)
+        txtStatusDef = findViewById(R.id.txtStatusPlayerDef)
+        txtStatusDiceCnt = findViewById(R.id.txtStatusPlayerDiceCnt)
+
+        btnStatus = findViewById(R.id.playerStatus)
+
         // 초기 상태
         playerChoose.setImageDrawable(null)
         enemyChoose.setImageDrawable(null)
@@ -82,12 +127,59 @@ class BattleActivity : AppCompatActivity() {
         btnRSPZone.visibility = View.VISIBLE
         diceZone.visibility = View.GONE
 
+        rewardZone = findViewById(R.id.rewardZone)
+        reward1 = findViewById(R.id.reward1)
+        reward2 = findViewById(R.id.reward2)
+        reward3 = findViewById(R.id.reward3)
+
+        // ⭐ HP Bar & Text
+        playerHpBar = findViewById(R.id.playerHpBar)
+        enemyHpBar = findViewById(R.id.enemyHpBar)
+        playerHpText = findViewById(R.id.playerHPText)
+        enemyHpText = findViewById(R.id.enemyHPText)
+
+        //상태창 업데이트 함수
+        // 초기 세팅
+        playerHpBar.max = playerMaxHP
+        enemyHpBar.max = enemyMaxHP
+
+        updateHpUI()
+        updateStatusUI()
+
         btnScissor.setOnClickListener { playRSP(0) }
         btnRock.setOnClickListener { playRSP(1) }
         btnPaper.setOnClickListener { playRSP(2) }
 
         // 플레이어 턴 버튼 리스너 (사용자 조작)
         btnRollDice.setOnClickListener { playerTurn() }
+
+        // 상태창 버튼 리스너
+        btnStatus.setOnClickListener { toggleStatusWindow() }
+    }
+
+    private fun updateStatusUI() {
+        txtStatusHP.text = "현재 HP: $playerHP"
+        txtStatusAtk.text = "추가 공격력: +$playerAtk"
+        txtStatusDef.text = "방어력: +$playerDef"
+        txtStatusDiceCnt.text = "주사위 개수: +$playerDiceRemain"
+    }
+
+    private fun toggleStatusWindow() {
+        if (isStatusOpen) {
+            statusWindow.visibility = View.GONE
+        } else {
+            statusWindow.visibility = View.VISIBLE
+        }
+        isStatusOpen = !isStatusOpen
+    }
+
+    // ⭐ HP Bar + 텍스트 즉시 갱신
+    private fun updateHpUI() {
+        playerHpBar.progress = playerHP
+        enemyHpBar.progress = enemyHP
+
+        playerHpText.text = "${playerHP} / ${playerMaxHP}"
+        enemyHpText.text = "${enemyHP} / ${enemyMaxHP}"
     }
 
     // 0:가위, 1:바위, 2:보
@@ -179,14 +271,11 @@ class BattleActivity : AppCompatActivity() {
     // ============================================================
     private fun playerTurn() {
         if (!isPlayerTurn) return                 // 내 턴 아니면 무시
-//        if (isDiceRolling) return                 // 연타 방지
         if (playerDiceRemain <= 0) return         // 남은 횟수 없으면 무시
 
-//        isDiceRolling = true
         btnRollDice.isEnabled = false
 
         Handler(Looper.getMainLooper()).postDelayed({
-            // 주사위 결과 처리는 공통 함수(rollDice_Common)로 수행
             rollDice()
         }, 500)
     }
@@ -203,7 +292,6 @@ class BattleActivity : AppCompatActivity() {
         // 적이 굴리는 동안 플레이어는 버튼 금지
         isPlayerTurn = false
         btnRollDice.isEnabled = false
-//        isDiceRolling = true
 
         Handler(Looper.getMainLooper()).postDelayed({
             // 공통 처리 호출 (isPlayerTurn이 false이므로 적 방식으로 처리됨)
@@ -237,7 +325,7 @@ class BattleActivity : AppCompatActivity() {
             if (enemyHP < 0) enemyHP = 0
 
             // 로그
-            whoFirst.text = "플레이어가 적에게 $damage 대미지를 줬습니다. (주사위 $diceNumber)"
+            whoFirst.text = "플레이어가 적에게 $damage 대미지를 줬습니다."
 
             // 남은 횟수 차감
             playerDiceRemain--
@@ -250,15 +338,14 @@ class BattleActivity : AppCompatActivity() {
             if (playerHP < 0) playerHP = 0
 
             // 로그
-            whoFirst.text = "적이 플레이어에게 $damage 대미지를 줬습니다. (주사위 $diceNumber)"
+            whoFirst.text = "적이 플레이어에게 $damage 대미지를 줬습니다."
 
             // 남은 횟수 차감
             enemyDiceRemain--
         }
 
-//        // 애니메이션/락 해제
-//        isDiceRolling = false
-
+        updateHpUI()
+        updateStatusUI()
         // 전투 상태 체크 (HP, 남은 주사위 등)
         checkBattleState()
     }
@@ -269,13 +356,18 @@ class BattleActivity : AppCompatActivity() {
     private fun checkBattleState() {
         // 승패 체크
         if (playerHP <= 0) {
-            whoFirst.text = "플레이어 패배!"
+            whoFirst.text = "패배!"
             btnRollDice.isEnabled = false
             return
         }
         if (enemyHP <= 0) {
-            whoFirst.text = "적 패배!"
+            whoFirst.text = "승리!"
             btnRollDice.isEnabled = false
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                showRewardSelection()
+            }, 3000)
+
             return
         }
 
@@ -304,7 +396,7 @@ class BattleActivity : AppCompatActivity() {
     }
 
 
-    // 플레이어 턴 종료 → 5초 후 적 턴
+    // 플레이어 턴 종료 → 3초 후 적 턴
     private fun endPlayerTurn() {
         isPlayerTurn = false
         btnRollDice.isEnabled = false
@@ -314,10 +406,10 @@ class BattleActivity : AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed({
             enemyTurn()
-        }, 5000)
+        }, 3000)
     }
 
-    // 적 턴 종료 → 5초 후 플레이어 턴
+    // 적 턴 종료 → 3초 후 플레이어 턴
     private fun endEnemyTurn() {
         isPlayerTurn = true
 
@@ -327,7 +419,46 @@ class BattleActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             // 플레이어가 다시 굴릴 수 있도록 버튼 활성화
             btnRollDice.isEnabled = true
-        }, 5000)
+        }, 3000)
+    }
+
+    private fun showRewardSelection() {
+        // 주사위 영역, 가위바위보 영역 숨기기
+//        diceZone.visibility = View.GONE
+//        btnRSPZone.visibility = View.GONE
+
+        // reward 3개 랜덤 추출
+        val picked = rewardList.shuffled().take(3)
+
+        // 이미지 설정
+        reward1.setImageResource(picked[0].img)
+        reward2.setImageResource(picked[1].img)
+        reward3.setImageResource(picked[2].img)
+
+        rewardZone.visibility = View.VISIBLE
+
+        // 클릭 리스너
+        reward1.setOnClickListener { applyReward(picked[0]) }
+        reward2.setOnClickListener { applyReward(picked[1]) }
+        reward3.setOnClickListener { applyReward(picked[2]) }
+    }
+
+    private fun applyReward(reward: Reward) {
+        when (reward.type) {
+            "hp" -> playerHP += reward.value
+            "atk" -> playerAtk += reward.value
+            "def" -> playerDef += reward.value
+            "dice" -> playerDiceRemain += reward.value
+        }
+
+        updateHpUI()
+        updateStatusUI()
+
+        // 보상창 숨기기
+        rewardZone.visibility = View.GONE
+
+        // 전투 종료 문구 출력
+        whoFirst.text = "뱀게임으로 복귀"
     }
 
 }
